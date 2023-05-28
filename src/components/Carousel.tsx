28611e5378165e9150f7ae6098a17d1285a2d197
Martin Devillers <martin@devillers.nl>
2023-05-28 18:21:44 -0400

    Fix invalid property

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -48,7 +48,6 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
       <link
         rel="stylesheet"
         type="text/css"
-        charSet="UTF-8"
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
       />
       <link
